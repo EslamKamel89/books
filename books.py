0f648a4e3f56adcd65f1896de7f03054eae88e59
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-12 17:23:52 +0300

    add Book class and books endpoint to FastAPI application

--- books.py
+++ books.py
@@ -2,6 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI() 
 
+class Book :
+    id: int 
+    title:str 
+    author:str
+    description:str
+    rating:int
+    def __init__(self , id :int , title:str, author:str , description:str  , rating :int) :
+        self.id = id 
+        self.title = title 
+        self.author = author 
+        self.description = description 
+        self.rating = rating 
+books:list[Book] = []
+
 @(app.get('/'))
 async def  home():
     return {'message' : "Welcome to fastapi books api"}
+
+@(app.get('/books'))
+async def books_index():
+    return books 
