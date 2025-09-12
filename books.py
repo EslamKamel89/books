2222734fc9b7eb4e67760a82fa50b111b9eed5bc
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-12 17:30:19 +0300

    populate books list with initial book entries

--- books.py
+++ books.py
@@ -14,7 +14,20 @@ class Book :
         self.author = author 
         self.description = description 
         self.rating = rating 
-books:list[Book] = []
+books:list[Book] = [
+    Book( 1, "The Silent Patient",  "Alex Michaelides" , 'description 1' , 3),
+    Book( 2, "Educated",  "Tara Westover" , 'description 2' , 4),
+    Book( 3, "Dune",  "Frank Herbert",  'description 3' , 2),
+    Book( 4, "Thinking, Fast and Slow",  "Daniel Kahneman" , 'description 4' , 3),
+    Book( 5, "The Hobbit",  "J.R.R. Tolkien" , 'description 5' , 3),
+    Book( 6, "Sapiens: A Brief History of Humankind",  "Yuval Noah Harari" , 'description 6' , 5),
+    Book( 7, "The Midnight Library",  "Matt Haig" , 'description 7' , 3),
+    Book( 8, "Atomic Habits",  "James Clear" , 'description 8' , 2),
+    Book( 9, "The Body: A Guide for Occupants",  "Bill Bryson" , 'description 9' , 1),
+    Book( 10 , "Pride and Prejudice",  "Jane Austen" , 'description 10' , 4),
+    Book( 11 , "The Seven Husbands of Evelyn Hugo",  "Taylor Jenkins Reid" , 'description 11' , 5),
+    Book( 12 , "Deep Work",  "Cal Newport" , 'description 12' , 5),
+]
 
 @(app.get('/'))
 async def  home():
